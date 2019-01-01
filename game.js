1998c2891fd574f5f28e7e15f1c5fe9e918d0592
Pál Földesi <29228103+pal-foldesi@users.noreply.github.com>
2019-01-01 08:14:25 +0100

    Added collision detection between ball and walls

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 import Ball from './ball.js';
 
-import { CANVAS, CONTEXT, BRICK_HEIGHT, BRICK_WIDTH } from './constants.js';
+import { CANVAS, CONTEXT, BRICK_HEIGHT, BRICK_WIDTH, BALL_RADIUS } from './constants.js';
 
 class Game {
     constructor() {
@@ -37,16 +37,24 @@ class Game {
     }
 
     tick() {
+        if (this.detectGameOver()) {
+            this.end();
+            return;
+        }
+
         this.ball.clear();
         this.ball.x += this.ball.dx;
         this.ball.y += this.ball.dy;
+        this.detectCollisions();
         this.ball.draw();
+
         const self = this;
         window.requestAnimationFrame(() => self.tick());
     }
 
     start() {
         Game.hideGameStartText();
+        this.ball.dx = -5;
         this.ball.dy = -5;
         const self = this;
         window.requestAnimationFrame(() => self.tick());
@@ -78,6 +86,39 @@ class Game {
         CONTEXT.strokeText('game area to', CANVAS.width / 2, CANVAS.height / 2);
         CONTEXT.fillText('start the game!', CANVAS.width / 2, CANVAS.height / 2 + 140);
         CONTEXT.strokeText('start the game!', CANVAS.width / 2, CANVAS.height / 2 + 140);
+    }
+
+    detectCollisions() {
+        //Ball and top
+        if (this.ball.y - BALL_RADIUS <= 0) {
+            this.ball.dy *= -1;
+        }
+
+        //Ball and sides
+        if (this.ball.x - BALL_RADIUS <= 0 || this.ball.x + BALL_RADIUS >= CANVAS.width) {
+            this.ball.dx *= -1;
+        }
+    }
+
+    detectGameOver() {
+        //Ball and bottom
+        if (this.ball.y >= CANVAS.height) {
+            this.end();
+            return true;
+        }
+        return false;
+    }
+
+    end() {
+        CONTEXT.fillStyle = 'khaki';
+        CONTEXT.font = 'bold 144px serif';
+        CONTEXT.lineWidth = 5;
+        CONTEXT.strokeStyle = 'tomato';
+        CONTEXT.textAlign = 'center';
+        CONTEXT.fillText('Game', CANVAS.width / 2, CANVAS.height / 2 - 80);
+        CONTEXT.strokeText('Game', CANVAS.width / 2, CANVAS.height / 2 - 80);
+        CONTEXT.fillText('over!', CANVAS.width / 2, CANVAS.height / 2 + 80);
+        CONTEXT.strokeText('over!', CANVAS.width / 2, CANVAS.height / 2 + 80);
     }
 }
 
