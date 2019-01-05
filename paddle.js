922e8977b63cd52b4484bd89b32315365e9502d2
Pál Földesi <29228103+pal-foldesi@users.noreply.github.com>
2019-01-05 12:57:54 +0100

    Fixed linting errors

--- paddle.js
+++ paddle.js
@@ -1,32 +1,33 @@
 import {
-  CONTEXT,
-  BRICK_WIDTH,
-  BRICK_HEIGHT,
-  PADDLE_HEIGHT
+    CONTEXT,
+    BRICK_WIDTH,
+    BRICK_HEIGHT,
+    PADDLE_HEIGHT,
 } from './constants.js';
 
 class Paddle {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.oldX = x;
-    this.acc = 0;
-    this.hue = Math.random() * 360;
-    this.saturation = Math.random() * 100;
-    this.luminosity = 20 + Math.random() * 60;
-    this.fillStyle = `hsla(${this.hue},${this.saturation}%,${this.luminosity}%,1.0)`;
-    this.draw();
-  }
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.oldX = x;
+        this.acc = 0;
+        this.hue = Math.random() * 360;
+        this.saturation = Math.random() * 100;
+        this.luminosity = 20 + Math.random() * 60;
+        this.fillStyle = `hsla(${this.hue},${this.saturation}%,${this.luminosity}%,1.0)`;
+        this.draw();
+    }
 
-  draw() {
-    CONTEXT.fillStyle = this.fillStyle;
-    CONTEXT.fillRect(this.x, this.y, BRICK_WIDTH, PADDLE_HEIGHT);
-  }
+    draw() {
+        CONTEXT.fillStyle = this.fillStyle;
+        CONTEXT.fillRect(this.x, this.y, BRICK_WIDTH, PADDLE_HEIGHT);
+    }
 
-  clear() {
-    CONTEXT.fillStyle = 'white';
-    CONTEXT.fillRect(this.x - 1, this.y - 1, BRICK_WIDTH + 1, BRICK_HEIGHT + 1); //Width and length are offset to get rid of artifacts
-  }
+    clear() {
+        CONTEXT.fillStyle = 'white';
+        // Width and length are offset to get rid of artifacts
+        CONTEXT.fillRect(this.x - 1, this.y - 1, BRICK_WIDTH + 1, BRICK_HEIGHT + 1);
+    }
 }
 
 export default Paddle;
